16699c97440b81ef5a82925575e104f321bcd818
Levi Hackwith <levi.hackwith@gmail.com>
2023-07-03 20:15:03 +0000

    Convert nsMock object into a getter function
    In order to prevent collisions between tests, I made nsMock into a
    function called getNsMock that returns a fresh nsMock object. This
    way, tests can guarantee that they're not modifying any values used by
    other tests.

--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from "@jest/globals";
-import nsMock from "../test/ns-mock";
+import getNsMock from "../test/ns-mock";
 import { findServers } from "./helpers";
 import { NS } from "../types";
 
 describe("findServers", () => {
     describe("When the root is defined as 'home'", () => {
         it("should recursively map the network from 'home'", () => {
-            const result = findServers(nsMock as NS, "home");
+            const result = findServers(getNsMock() as NS, "home");
             const expectedMap = [
                 "foodnstuff",
                 "max-hardware",
@@ -20,7 +20,7 @@ describe("findServers", () => {
 
     describe("When the root is defined as 'n00dles'", () => {
         it("should recursively map the network from 'n00dles'", () => {
-            const result = findServers(nsMock as NS, "n00dles");
+            const result = findServers(getNsMock() as NS, "n00dles");
             const expectedMap = ["max-hardware", "zero"];
 
             expect(result).toStrictEqual(expectedMap);
@@ -29,7 +29,7 @@ describe("findServers", () => {
 
     describe("When the root is defined as 'max-hardware'", () => {
         it("should recursively map the network from 'max-hardware'", () => {
-            const result = findServers(nsMock as NS, "max-hardware");
+            const result = findServers(getNsMock() as NS, "max-hardware");
             const expectedMap: Array<string> = [];
 
             expect(result).toStrictEqual(expectedMap);
--- test/ns-mock.ts
+++ test/ns-mock.ts
@@ -1,16 +1,21 @@
 import { NS } from "../types";
 
-const nsMock: Partial<NS> = {
-    scan: (host?: string | undefined): string[] => {
-        switch (host) {
-            case "home":
-                return ["n00dles", "foodnstuff"];
-            case "n00dles":
-                return ["home", "zero", "max-hardware"];
-            default:
-                return ["n00dles"];
+const getNsMock = (args?: (string | number | boolean)[]) => {
+    const nsMock: Partial<NS> = {
+        args: args,
+        scan: (host?: string | undefined): string[] => {
+            switch (host) {
+                case "home":
+                    return ["n00dles", "foodnstuff"];
+                case "n00dles":
+                    return ["home", "zero", "max-hardware"];
+                default:
+                    return ["n00dles"];
+            }
         }
-    }
+    };
+
+    return nsMock as NS;
 };
 
-export default nsMock;
+export default getNsMock;

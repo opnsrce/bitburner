c9528e3efb84f7ec7b810bb908b4e59b9901509c
Levi Hackwith <levi.hackwith@gmail.com>
2023-07-04 17:12:04 +0000

    Document ScriptConfig properties

--- types/script-config.d.ts
+++ types/script-config.d.ts
@@ -1,5 +1,9 @@
+/** Configuration object returned by parseNetScriptArgs. */
 export type ScriptConfig = {
+    /** Contains unrecognized arguments passed to the script. */
     args: string[];
+    /** The target server the script will run against / execute on. */
     target: string;
+    /** If true, script will keep running after main operation finishes. */
     keepOpen: boolean;
 };

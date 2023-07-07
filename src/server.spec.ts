139239bbf21a482902e37b389ab16867d1f5b40a
Levi Hackwith <levi.hackwith@gmail.com>
2023-07-07 16:55:04 +0000

    Call ns.getServer in the constructor

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -1,11 +1,30 @@
+import { describe, expect, it, jest, afterEach } from "@jest/globals";
+import getNsMock from "../test/ns-mock";
 import Server from "./server";
+import { NS } from "../types";
 
 describe("Server", () => {
     describe("constructor", () => {
-        it("should return an instance of Server", () => {
-            const server = new Server();
+        let ns: NS;
 
-            expect(server).toBeInstanceOf(Server);
+        describe("When a server name is defined", () => {
+            beforeEach(() => {
+                ns = getNsMock();
+
+                jest.spyOn(ns, "getServer");
+            });
+
+            afterEach(() => {
+                jest.clearAllMocks();
+            });
+
+            it("should call ns.getServer with the server name", () => {
+                const serverName = "n00dles";
+
+                new Server(ns, serverName);
+
+                expect(ns.getServer).toBeCalledWith(serverName);
+            });
         });
     });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-export default class Server {}
+import { NS } from "../types";
+
+export default class Server {
+    private _ns: NS;
+    constructor(ns: NS, name: string) {
+        this._ns = ns;
+
+        this._ns.getServer(name);
+    }
+}
